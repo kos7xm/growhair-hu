c3f3070a143b721d5653ac0f0450474234ec4959
Kos7xm <kos7xm@gmail.com>
2021-07-12 12:34:07 +0300

    new token

--- addrepo.sh
+++ addrepo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NAME="$1"
 
-curl  https://api.github.com/user/repos -d '{"name":"'$NAME'"}' -H "Authorization: token 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"
+curl  https://api.github.com/user/repos -d '{"name":"'$NAME'"}' -H "Authorization: token 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"
 
 git init
 git add -A
